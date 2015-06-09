445c4912900784153d0e35cf0bb4e184d91bc072
Karan Vahi <vahi@isi.edu>
2015-06-09 00:18:15 +0000

    fail on error is enabled only on the second instance
    also the reference 1000X files should be assumed to be in the
    directory where the job is launched from
    
    
    
    git-svn-id: https://pegasus.isi.edu/svn/page/trunk/workflow/imputation2@656 f4e34a31-411c-4976-93cd-739bbfecc312

--- scripts/phase-shapeit.sh
+++ scripts/phase-shapeit.sh
@@ -11,8 +11,6 @@
 #$2: number of chromosome – “22”
 #$3: directory name – “chr22”
 
-#fail on first error
-set -e
 
 # set -x to see on terminal what commands are executed
 # only for debugging
@@ -29,14 +27,18 @@ study_name=$1
 chromosome_num=$2
 directory=$3
 
-shapeit --input-vcf ./$3/$1.chr$2.recode.vcf.gz --input-ref ./1000GP_Phase3/1000GP_Phase3_chr$2.hap.gz ./1000GP_Phase3/1000GP_Phase3_chr$2.legend.gz ./1000GP_Phase3/1000GP_Phase3.sample --input-map ./1000GP_Phase3/genetic_map_chr$2_combined_b37.txt --thread 1 --exclude-snp ./$3/$1.chr$2.duplicate.snp.site.out --effective-size 20000 --output-max ./$3/$1.phase.chr$2.haps ./$3/$1.phase.chr$2.sample --output-log ./$3/$1.phase.chr$2.log
+shapeit --input-vcf ./$3/$1.chr$2.recode.vcf.gz --input-ref ./1000GP_Phase3_chr$2.hap.gz ./1000GP_Phase3_chr$2.legend.gz ./1000GP_Phase3.sample --input-map ./genetic_map_chr$2_combined_b37.txt --thread 1 --exclude-snp ./$3/$1.chr$2.duplicate.snp.site.out --effective-size 20000 --output-max ./$3/$1.phase.chr$2.haps ./$3/$1.phase.chr$2.sample --output-log ./$3/$1.phase.chr$2.log
 
+# we test that shapeit ran successfully only on basis of presence of
+# error statement in it's log
 if grep ERROR ./$3/$1.phase.chr$2.log
 then
-        echo "Errors detected in ./$3/$1.phase.chr$2.log"
-	cat ./$3/$1.phase.chr$2.snp.strand.exclude ./$3/$1.chr$2.duplicate.snp.site.out > ./$3/$1.chr$2.duplicate.snp.site.out.new
-	mv ./$3/$1.chr$2.duplicate.snp.site.out.new ./$3/$1.chr$2.duplicate.snp.site.out
-	shapeit --input-vcf ./$3/$1.chr$2.recode.vcf.gz --input-ref ./1000GP_Phase3/1000GP_Phase3_chr$2.hap.gz ./1000GP_Phase3/1000GP_Phase3_chr$2.legend.gz ./1000GP_Phase3/1000GP_Phase3.sample --input-map ./1000GP_Phase3/genetic_map_chr$2_combined_b37.txt --thread 1 --exclude-snp ./$3/$1.chr$2.duplicate.snp.site.out --effective-size 20000 --output-max ./$3/$1.phase.chr$2.haps ./$3/$1.phase.chr$2.sample --output-log ./$3/$1.phase.chr$2.log
-
+    #fail on first error 
+    set -e
+    echo "Errors detected in ./$3/$1.phase.chr$2.log"
+    cat ./$3/$1.phase.chr$2.snp.strand.exclude ./$3/$1.chr$2.duplicate.snp.site.out > ./$3/$1.chr$2.duplicate.snp.site.out.new
+    mv ./$3/$1.chr$2.duplicate.snp.site.out.new ./$3/$1.chr$2.duplicate.snp.site.out
+    shapeit --input-vcf ./$3/$1.chr$2.recode.vcf.gz --input-ref ./1000GP_Phase3_chr$2.hap.gz ./1000GP_Phase3_chr$2.legend.gz ./1000GP_Phase3.sample --input-map ./genetic_map_chr$2_combined_b37.txt --thread 1 --exclude-snp ./$3/$1.chr$2.duplicate.snp.site.out --effective-size 20000 --output-max ./$3/$1.phase.chr$2.haps ./$3/$1.phase.chr$2.sample --output-log ./$3/$1.phase.chr$2.log
+    
 fi
 
