6a34ff2d099c56f22693d088d474d99375e72313
Lisheng Zhou <Zhou@dls.rutgers.edu>
2015-06-20 20:22:08 +0000

    changed run_preprocessing.sh
    git-svn-id: https://pegasus.isi.edu/svn/page/trunk/workflow/imputation2@678 f4e34a31-411c-4976-93cd-739bbfecc312

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -4,32 +4,34 @@
 CDIR='pwd'
 
 file="BioMe-AA_ILLUMINA"
-study="chr22"
+num=22
+study="testing"
+node="chr22"
 
 echo "SNP names update"
 ## bash snp_names_update.sh input_prefix output
-bash snp_names_update.sh $file ${study}-result
+bash snp_names_update.sh $file ${study}-${num}-result
 
 echo "monomorphic SNPs"
-Rscript --vanilla more-alleles-2015-05-25.R ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz ${study}-result ${study}-result2
+Rscript --vanilla more-alleles-2015-05-25.R ALL.chr${num}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz ${study}-${num}-result ${study}-${num}-result2
 
 echo "liftOver"
-bash liftover_to_37.sh ${study}-result2
+bash liftover_to_37.sh ${study}-${num}-result2
 
 
 echo "HRC process step"
-perl HRC-check-bim.pl ${study}-result2-final.bim
+perl HRC-check-bim.pl ${study}-${num}-result2-final.bim
 
 echo "HRC 2nd step"
 bash Run-plink.sh
-rm *-${study}-result2-final.bim
+rm *-${study}-${num}-result2-final.bim
 ## generate xxx-updated.bim
 
 echo "convert into VCF"
-mkdir $study
-mv ${study}-result2-final-updated* $study/
-cd $study
-plink-1.9 --bfile ${study}-result2-final-updated --recode vcf --out $file
+mkdir $study-${num}
+mv ${study}-${num}-result2-final-updated* $study-${num}/
+cd $study-${num}
+plink-1.9 --bfile ${study}-${num}-result2-final-updated --recode vcf --out $file
 gzip $file.vcf
 cd ..
 
